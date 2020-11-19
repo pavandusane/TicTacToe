fa08eed6e086858e5c9f1a9c128819d242f14f6e
pavandusane <dusanepv7@gmail.com>
2020-11-19 12:19:23 +0530

    added : display the board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -6,34 +6,48 @@ public class TicTacToeGame {
 	
 	//UC1 creating board and initialize with blank spaces
 	public static char[] createBoard(){
-		char[] board=new char[10];
+		char[] board=new char[9];
 		for (int index = 1; index < board.length; index++) {
 			board[index]=' ';
 		}
 		return board;
 	}
 	
-	public void choosePlayer() {
+	//UC2 creating method to allow user to choose letter
+	public void characterChoose() {
 		Scanner sc=new Scanner(System.in);
 		char compMark='x';
 		System.out.println("Enter your mark (X or O) : ");
-		char usermark=sc.next().charAt(0);
-		if (usermark=='x'||usermark=='X') {
+		char userMark=sc.next().charAt(0);
+		if (userMark=='x'||userMark=='X') {
 			compMark='O';
-		}else if (usermark=='o'||usermark=='O') {
+		}else if (userMark=='o'||userMark=='O') {
 			compMark='X';
-		}else
+		}else {
 			System.out.println("Invalid input mark");
-		System.out.println("User Marks is :"+usermark+" Computer Marks is :"+compMark);
+			}
 	}
+	
+	//UC3 display the board
+	public void displayBoard(char[] show) {
+		for (int i = 0; i < show.length; i++) {
+			if (i==3||i==6) {
+				System.out.println();
+			}
+			System.out.print(show[i]+"|");
+		}
+	}
+	
+	
 	
 	
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
-		char[] board=createBoard();
-		TicTacToeGame TicTacToeGameObj=new TicTacToeGame();
-		TicTacToeGameObj.choosePlayer();
+		char[] showBoard=createBoard();
+		TicTacToeGame gameObj=new TicTacToeGame();
+		gameObj.characterChoose();
+		gameObj.displayBoard(showBoard);
 	}
 
 }
