9744c8734b5773a22b4fd592887d683dc2d26485
pavandusane <dusanepv7@gmail.com>
2020-11-19 10:18:21 +0530

    Added : create board method is added

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,20 @@
 package com.game.tictactoe;
 
 public class TicTacToeGame {
-
+	
+	public static char[] createBoard(){
+		char[] board=new char[10];
+		for (int index = 0; index < board.length; index++) {
+			board[index]=' ';
+		}
+		return board;
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
+		char[] board1=new char[10];
+		board1=createBoard();
 	}
 
 }
